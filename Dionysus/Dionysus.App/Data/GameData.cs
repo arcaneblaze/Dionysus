85413140c3f8d4dd87d731d15c3f49fc54ed538a
blazor911 <kl.kiruxa2006@gmail.com>
2024-10-08 18:00:53 +0300

    Add files via upload

---
+++ Dionysus/Dionysus.App/Data/GameData.cs
@@ -0,0 +1,53 @@
+using Dionysus.App.Models;
+using Newtonsoft.Json;
+
+namespace Dionysus.App.Data;
+
+public class GameData
+{
+    public class GamesData
+    {
+        private static string _josnPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/games.json");
+        public static IEnumerable<GameModel> ParseGamesFromJSON()
+        {
+            try
+            {
+                if (!Directory.Exists("Data")) Directory.CreateDirectory("Data");
+                
+                if (!File.Exists(_josnPath))
+                { 
+                    File.WriteAllText(_josnPath, "[]"); 
+                }
+                
+                var jsonData = File.ReadAllText(_josnPath);
+                
+                var gamesList = JsonConvert.DeserializeObject<List<GameModel>>(jsonData);
+                Console.WriteLine($"{_josnPath} Parsed");
+                return gamesList ?? new List<GameModel>();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"File not found: {ex.FileName}");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred while parsing the JSON file: {ex.Message}");
+            }
+            
+            return new List<GameModel>();
+        }
+
+        public static async Task SaveToJSON(List<GameModel> gamesList)
+        {
+            var directory = Path.GetDirectoryName(_josnPath);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+
+            }
+            var json = JsonConvert.SerializeObject(gamesList, Formatting.Indented);
+            await File.WriteAllTextAsync(_josnPath, json);
+        }
+    }
+}
---
+++ Dionysus/Dionysus.App/WebScrap/GOGScrapper/GOG.cs
@@ -0,0 +1,119 @@
+﻿using craftersmine.SteamGridDBNet;
+using Dionysus.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Dionysus.WebScrap.GOGScrapper;
+
+public class GOG
+{
+    public static async Task<bool> GetStatus()
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://freegogpcgames.com");
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Website https://freegogpcgames.com is available. Status: {response.StatusCode}");
+                    return true; 
+                }
+                else
+                {
+                    Console.WriteLine($"Website https://freegogpcgames.com is unavailable. Status: {response.StatusCode}");
+                    return false; 
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false; 
+        }
+    }
+    
+    public static async Task<IEnumerable<SearchGameInfoStruct>> GetSearchResponse(string _request)
+{
+    var _siteLink = $"https://freegogpcgames.com/?s={_request}";
+    var _responseList = new List<SearchGameInfoStruct>();
+    try
+    {
+        using var _httpClient = new HttpClient();
+        var _html = await _httpClient.GetStringAsync(_siteLink);
+        var _htmlDocument = new HtmlAgilityPack.HtmlDocument();
+        _htmlDocument.LoadHtml(_html);
+
+        var _responseDivs = _htmlDocument.DocumentNode.SelectNodes("//div[contains(@class, 'generate-columns-container')]/article");
+        if (_responseDivs != null)
+        {
+            var tasks = _responseDivs.Select(async _div =>
+            {
+                var _name = _div.SelectSingleNode(".//header/h2/a");
+                var _link = _name.Attributes["href"].Value;
+
+                var _rephrasedName = _name.InnerText.Trim()
+                    .Replace("&#8211;", "-")
+                    .Replace("&#038;", "&")
+                    .Replace("&#8217;", "`")
+                    .Replace(":", "")
+                    .Replace("-", "");
+                var _rephrasedRequest = _request.Replace(":", "").Replace("-", "");
+
+                var (downloadLink, size) = await GetDataFromLink(_link);
+
+                if (_rephrasedName.ToLower().Contains(_rephrasedRequest.ToLower()))
+                {
+                    var _downloadLink = BypassDownloadLink(downloadLink);
+                    var gameA = await GamesPage._steamGridDb.SearchForGamesAsync(_rephrasedName);
+                    var icons = await GamesPage._steamGridDb.GetGridsForGameAsync(gameA[0], dimensions: SteamGridDbDimensions.W920H430);
+                    var imageUrl = icons[0].FullImageUrl;
+                    _responseList.Add(new SearchGameInfoStruct()
+                    {
+                        Cover = imageUrl,
+                        Name = _rephrasedName,
+                        Link = _link,
+                        Size = size.Replace("Size: ", "").Replace("GiB", "GB"),
+                        DownloadLink = _downloadLink
+                    });
+                }
+            });
+
+            await Task.WhenAll(tasks);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+    return _responseList;
+}
+
+private static async Task<(string downloadLink, string size)> GetDataFromLink(string _link)
+{
+    using var _httpClient = new HttpClient();
+    var _html = await _httpClient.GetStringAsync(_link);
+    var _htmlDocument = new HtmlAgilityPack.HtmlDocument();
+    _htmlDocument.LoadHtml(_html);
+
+    var _downloadLink = _htmlDocument.DocumentNode
+        .SelectSingleNode("//a[contains(@class, 'download-btn')]").Attributes["href"].Value;
+
+    var _size = _htmlDocument.DocumentNode
+        .SelectSingleNode("//div[contains(@class, 'inside-article')]/div[1]/p[6]/em").InnerText
+        .Trim();
+
+    return (_downloadLink, _size);
+}
+
+private static string BypassDownloadLink(string url)
+{
+    using var _client = new HttpClient();
+    var _htmlString = _client.GetStringAsync(url).Result;
+    var _document = new HtmlAgilityPack.HtmlDocument();
+    _document.LoadHtml(_htmlString);
+    return _document.DocumentNode.SelectSingleNode("//a[contains(@class, 'button')]")
+        .Attributes["href"].Value.Replace("&amp;", "&").Replace("&#038;", "&");
+}
+}
